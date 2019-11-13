cedab0c7ab9da3bad816d8d0f3baef76f890d5c5
Nagarjun <nagarjun.bn@techjini.com>
2019-11-13 17:15:01 +0530

    changes

--- resources/views/chat/index.blade.php
+++ resources/views/chat/index.blade.php
@@ -1,11 +1,58 @@
-<script type="text/javascript">
-	var conn = new WebSocket('ws://laravel.local:9000');
-conn.onopen = function(e) {
-    console.log("Connection established!");
-    conn.send('Hello World!');
-};
-
-conn.onmessage = function(e) {
-    console.log(e.data);
-};
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Chat</title>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+<h1>Chat</h1>
+<p>Available Users</p>
+<table>
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($users as $user)
+        <tr>
+            <td>{{$user->id}}</td>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}
+            <td>
+                <button class="select-user-button" data-userID="{{$user->id}}" data-userName="{{$user->name}}"
+                        data-userEmail="{{$user->email}}">
+                    Select User
+                </button>
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+<script>
+    $('.select-user-button').click(function (event) {
+        console.log('here');
+        // console.log($(this).attr("data-userID"));
+        console.log($(this).data('userID'));
+        console.log($(this).data('userName'));
+        console.log($(this).data('userEmail'));
+    });
 </script>
+<script type="text/javascript">
+    var conn = new WebSocket('ws://laravel.local:9000');
+    conn.onopen = function (e) {
+        console.log("Connection established!");
+        conn.send('Hello World!');
+    };
+
+    conn.onmessage = function (e) {
+        console.log(e.data);
+    };
+</script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/chat', function () {
-    return view('chat.index');
+    $users = App\User::all();
+    return view('chat.index',['users' => $users]);
 });
